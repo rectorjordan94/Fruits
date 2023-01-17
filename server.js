670b9d3e987df63a7c56c1abe88cd28bb86b6839
rectorjordan94 <rectorjordan94@gmail.com>
2023-01-17 10:52:52 -0600

    adds fruit model and seed route

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-///
-// Import Dependencies
-///
+
+//! Import Dependencies
+
 const express = require('express') // import the express framework
 const mongoose = require('mongoose') // import the mongoose library
 const morgan = require('morgan') // import the morgan request logger
 require('dotenv').config() // load my ENV file's variables
+const path = require('path') // import path module
 
-///
-//  Database Connection
-///
+//! Import Our Models
+const Fruit = require('./models/fruit')
+
+//!  Database Connection
 // this is where we will set up our inputs for our database connect function
 const DATABASE_URL = process.env.DATABASE_URL
 // here is our DB config object
@@ -24,18 +26,55 @@ mongoose.connection
     .on('close', () => console.log('disconnected from mongoose'))
     .on('error', (err) => console.log('an error occurred: \n', err))
 
-///
-// Create our Express App Object
-///
+
+//! Create our Express App Object
+
 const app = express()
 
 
+//! Middleware
+// middleware runs before all the routes
+// every request is processed through our middleware before mongoose can do anything with it
+app.use(morgan('dev')) // this is for request logging, the 'tiny' argument declares what size of morgan log to use
+app.use(express.urlencoded({extended: true})) // this parses urlEncoded request bodies(useful for POST and PUT requests)
+app.use(express.static('public')) // this serves static files from the 'public' folder
+app.use(express.json()) // parses incoming request payloads with JSON
 
 
+//! Routes
 
+app.get('/', (req, res) => {
+    res.send('Server is live, ready for requests')
+})
 
-///
-// Server Listener
-///
+// we're going to build a seed route
+// this will seed the database for us with a few starter resources
+// there are two ways we will talk about seeding the database
+// First -> seed route, they work but they are not best practices
+// Second -> seed script, they work and they ARE best practices
+app.get('/fruits/seed', (req, res) => {
+    // array of starter resources(fruits)
+    const startFruits = [
+        { name: 'Orange', color: 'orange', readyToEat: true },
+        { name: 'Grape', color: 'purple', readyToEat: true },
+        { name: 'Banana', color: 'green', readyToEat: false },
+        { name: 'Strawberry', color: 'red', readyToEat: false},
+        { name: 'Coconut', color: 'brown', readyToEat: true},
+    ]
+    // then we delete every fruit in the database(all instances of this resource)
+    Fruit.deleteMany({})
+        .then(() => {
+            // then we'll seed(create) our starter fruits
+            Fruit.create(startFruits)
+                // tell our db what to do with success and failures
+                .then(data => {
+                    res.json(data)
+                })
+                .catch(err => console.log('the following error occurred: \n',err))
+        })
+})
+
+//! Server Listener
+
 const PORT = process.env.PORT
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
