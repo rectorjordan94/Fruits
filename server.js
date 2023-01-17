301d0b50628f496c49dfdfda8369fe52637f8b7d
rectorjordan94 <rectorjordan94@gmail.com>
2023-01-17 13:32:30 -0600

    update route for fruits working

--- server.js
+++ server.js
@@ -81,21 +81,6 @@ app.get('/fruits', (req, res) => {
         .catch(err => console.log('the following error occurred: \n', err))
 })
 
-//  SHOW route
-// Read -> finds and displays a single resource
-app.get('/fruits/:id', (req, res) => {
-    // get the id -> save to a variable
-    const id = req.params.id
-    // use a mongoose method to find using that id
-    Fruit.findById(id)
-        .then(fruit => {
-            // send the fruit as json upon success
-            res.json({fruit: fruit})
-        })
-    .catch(err => console.log(err))
-    // catch any errors
-})
-
 // CREATE route
 // Create -> receives a request body, and creates a new document in the database
 app.post('/fruits', (req, res) => {
@@ -111,6 +96,47 @@ app.post('/fruits', (req, res) => {
         // send an error if one occurs
         .catch(err => console.log(err))
 })
+
+// PUT route
+// Update -> updates a specific fruit
+//  PUT replaces the entire document with a new document from the req.body
+// PATCH is able to update specific fields at specific times, but it requires a little more code to ensure that it works properly, so we'll use that later
+app.put('/fruits/:id', (req, res) => {
+    // save the id to a variable for easy use later
+    const id = req.params.id
+    // save the request body to a variable for easy reference later
+    const updatedFruit = req.body
+    // use the mongoose method findByIdAndUpdate
+    // eventually we'll change how this route works, but for now, we'll do everything in one shot, with findByIdAndUpdate
+    Fruit.findByIdAndUpdate(id, updatedFruit, { new: true })
+        .then(fruit => {
+            console.log('the newly updated fruit: \n', fruit)
+            // update success message will just be a 204 - no content
+            res.sendStatus(204)
+        })
+        .catch(err => console.log(err))
+})
+
+
+// DELETE route
+// Delete -> delete a specific fruit
+
+//  SHOW route
+// Read -> finds and displays a single resource
+app.get('/fruits/:id', (req, res) => {
+    // get the id -> save to a variable
+    const id = req.params.id
+    // use a mongoose method to find using that id
+    Fruit.findById(id)
+        .then(fruit => {
+            // send the fruit as json upon success
+            res.json({fruit: fruit})
+        })
+    .catch(err => console.log(err))
+    // catch any errors
+})
+
+
 
 
 //! Server Listener
