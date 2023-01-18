05c7b01e2742e1c35920f620c1e8a5dce863dce0
rectorjordan94 <rectorjordan94@gmail.com>
2023-01-18 13:08:09 -0600

    sessions and user login/logout working

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -7,7 +7,7 @@ const bcrypt = require('bcryptjs')
 const router = express.Router()
 
 //! Routes
-// POST -> /users/signup
+//* POST -> /users/signup
 // this route creates new users in our db
 router.post('/signup', async (req, res) => {
     // this route will take a req.body and use that data to create a user
@@ -35,8 +35,61 @@ router.post('/signup', async (req, res) => {
         })
 })
 
+//* POST -> /users/login
+// this route creates new session in our db(and in the browser)
+router.post('/login', async (req, res) => {
+    // first we want to destructure the username & password from our req.body
+    const { username, password } = req.body
+    // search the db, for a user with a specific username
+    User.findOne({ username })
+        .then(async (user) => {
+            // we check if that user exists
+            if (user) {
+                // if they do, we compare the passwords using bcrypt
+                // bcrypt.compare -> evaluates to a truthy or a falsy value
+                // we'll save that result to a variable for easy reference later
+                // password -> comes from req.body
+                // user.password -> is saved in the database
+                const result = await bcrypt.compare(password, user.password)
 
+                if (result) {
+                    // if the passwords match, place the user's info in the session
+                    // this is where we use that session object that lives in our request object
+                    req.session.username = username
+                    req.session.loggedIn = true
+                    req.session.userId = user.id
 
+                    // console.log('this is req.session: \n', req.session)
+
+                    // we'll send a 201 response and the user as json(for now)
+                    // we'll update this after a couple tests to adhere to best practices
+                    res.status(201).json({ username: user.username})
+                } else {
+                    // if the passwords don't match, send the user a message
+                    res.json({error: 'username or password is incorrect'})
+                }
+            } else {
+                // if the user does not exist, we respond with a message saying so
+                res.json({error: 'user does not exist'})
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.json(err) // prevents requests from hanging if there is an error
+        })
+    
+})
+
+//* DELETE -> /users/logout
+// this route destroys a session in our db(and in the browser)
+router.delete('/logout', (req, res) => {
+    // destroy the session and send an appropriate response
+    req.session.destroy(() => {
+        console.log('this is req.session upon logout \n', req.session)
+        // eventually we will redirect users here, but thats after adding the view layer
+        res.sendStatus(204)
+    })
+}) 
 
 //! Export Router
 module.exports = router
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,6 +1,9 @@
 //! Import Dependencies
 const express = require('express') // import the express framework
 const morgan = require('morgan') // import the morgan request logger
+const session = require('express-session') // import the express-session package
+const MongoStore = require('connect-mongo') // import the connect-mongo package(for sessions)
+require('dotenv').config() // necessary because connect=mongo needs to access the database
 
 //! Middleware Function
 // Now, instead of processing our middleware in server.js, we're going to build a function that will take the entire app as an argument, and run requests through all of our middleware
@@ -11,6 +14,23 @@ const middleware = (app) => {
     app.use(express.urlencoded({extended: true})) // this parses urlEncoded request bodies(useful for POST and PUT requests)
     app.use(express.static('public')) // this serves static files from the 'public' folder
     app.use(express.json()) // parses incoming request payloads with JSON
+    // here, we set up and utilize a session function, and we pass that function a config argument, to configure our session in the way we want. This argument will tell express-session how to create and store our session
+    // the config object, needs several keys in order to work(see express-session docs)
+    // the keys are:
+    // secret -> a super top secret code, that allows for the creation of a session
+    // this secret is kinda like authorization, that allows our app to create with connect-mongo
+    // store -> tells connect-mongo where to save the session(our db)
+    // then two options: saveUninitialized(set to true) and resave(set to false)
+    app.use(
+        session({
+            secret: process.env.SECRET,
+            store: MongoStore.create({
+                mongoUrl: process.env.DATABASE_URL
+            }),
+            saveUninitialized: true,
+            resave: false
+        })
+    )
 }
 
 //! Export Middleware Function
